cc1985c4991aa5ecafe6f90aabe77ea3988c218d
ahmet479 <115145728+ahmet479@users.noreply.github.com>
2022-11-16 22:33:42 +0100

    Add files via upload

---
+++ JavaDers17/MainClass.java
@@ -0,0 +1,42 @@
+package JavaDers17;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		/*String isim = "Feride";
+		String isim1 = "Zulal";
+		boolean esitMi = isim.equals(isim1);
+		System.out.println(esitMi);*/
+		
+		/*String isim = "Feride";
+		String isim1 = "FERIDE";
+		boolean esitMi = isim.equals(isim1);
+		System.out.println(esitMi);*/
+		
+		/*String isim = "Feride";
+		String isim1 = "FERIDE";
+		boolean esitMi = isim.equals(isim1);
+		System.out.println(esitMi);*/
+		
+		/*String isim = "Feride";
+		String isim1 = "feride123";
+		boolean esitMi = isim.equals(isim1);
+		System.out.println(esitMi);*/
+		
+		/*String isim = "Feride";
+		String isim1 = "FERIDE";
+		boolean esitMi = isim.equalsIgnoreCase(isim1);
+		System.out.println(esitMi);*/
+		/*equalsIgnoreCase: Buyuk harf ve buyuk duyarliligini ortadan kaldirir.*/
+		
+		String bilgi = "Java Programlama Dili";
+			System.out.println(bilgi.toUpperCase());
+			System.out.println(bilgi.toLowerCase());
+			/*toUpperCase:Kucuk harflri buyuk harf yapmak
+			toLowerCase:Buyuk harflri kucukk harf yapmak*/
+			
+		
+		
+	}
+
+}
